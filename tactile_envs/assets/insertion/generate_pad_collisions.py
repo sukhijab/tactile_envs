f2530506fcdcbfa87b8f317dd150ac69d8a4353f
sukhijab <sukhijab@student.ethz.ch>
2024-10-28 11:24:15 -0700

    updated xml and pad generation script to include name

--- tactile_envs/assets/insertion/generate_pad_collisions.py
+++ tactile_envs/assets/insertion/generate_pad_collisions.py
@@ -1,4 +1,3 @@
-
 from absl import app, flags
 import shutil
 
@@ -8,29 +7,44 @@ flags.DEFINE_integer('ny', 32, 'Number of pads in y direction')
 
 
 def main(_):
-        
     dx = FLAGS.nx
     dy = FLAGS.ny
 
-    size_x = 0.011/dx
-    size_y = 0.009375*2/dy
-            
+    size_x = 0.011 / dx
+    size_y = 0.009375 * 2 / dy
+
     f = open("tactile_envs/assets/insertion/right_pad_collisions.xml", "w")
     f.write("<mujoco>\n")
     for i in range(dy):
-        pos_y = size_y + 2*size_y*i
+        pos_y = size_y + 2 * size_y * i
         for j in range(dx):
+            pos_x = -0.011 + size_x + 2 * size_x * j
 
-            pos_x = -0.011 + size_x + 2*size_x*j
-            
-            rgb = 0.6 + 0.1 * (i*dx + j)/(dx*dy - 1)
+            rgb = 0.6 + 0.1 * (i * dx + j) / (dx * dy - 1)
 
-            xml_string = "<geom class=\"pad\" pos=\"{} -0.0026 {}\" size=\"{} 0.004 {}\" rgba=\"{} {} {} 1\"/>".format(pos_x, pos_y, size_x, size_y, rgb, rgb, rgb)
-            
+            xml_string = "<geom class=\"pad\" pos=\"{} -0.0026 {}\" size=\"{} 0.004 {}\" rgba=\"{} {} {} 1\" name=\"rpad_geom{}\"/>".format(
+                pos_x, pos_y, size_x, size_y, rgb, rgb, rgb, i * dx + j)
+
             f.write(xml_string + '\n')
     f.write("</mujoco>")
     f.close()
-    shutil.copyfile("tactile_envs/assets/insertion/right_pad_collisions.xml", "tactile_envs/assets/insertion/left_pad_collisions.xml")
+
+    f = open("tactile_envs/assets/insertion/left_pad_collisions.xml", "w")
+    f.write("<mujoco>\n")
+    for i in range(dy):
+        pos_y = size_y + 2 * size_y * i
+        for j in range(dx):
+            pos_x = -0.011 + size_x + 2 * size_x * j
+
+            rgb = 0.6 + 0.1 * (i * dx + j) / (dx * dy - 1)
+
+            xml_string = "<geom class=\"pad\" pos=\"{} -0.0026 {}\" size=\"{} 0.004 {}\" rgba=\"{} {} {} 1\" name=\"lpad_geom{}\"/>".format(
+                pos_x, pos_y, size_x, size_y, rgb, rgb, rgb, i * dx + j)
+
+            f.write(xml_string + '\n')
+    f.write("</mujoco>")
+    f.close()
+    # shutil.copyfile("tactile_envs/assets/insertion/right_pad_collisions.xml", "tactile_envs/assets/insertion/left_pad_collisions.xml")
 
     touch_sensor_string = """
     <mujoco>
